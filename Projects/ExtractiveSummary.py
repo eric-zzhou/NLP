392643dec6ab439ea56f37138f60be7967e13fee
ericzhou16 <ericsuper@outlook.com>
2022-10-19 23:04:00 -0700

    Finished extractive summary

--- Projects/ExtractiveSummary.py
+++ Projects/ExtractiveSummary.py
@@ -4,19 +4,25 @@
 import nltk
 import re
 import heapq
-import numpy as np
-import pandas as pd
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as bs
 from nltk import word_tokenize, FreqDist
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords, reuters
+from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import defaultdict
 
 # Constants and user input
-MAX_SENT_LEN = 30
+MAX_SENT_LEN = 25
+DEFAULT_LINK = "https://www.hcn.org/issues/138/barry-lopez-we-are-shaped-by-the-sound-of-wind-the-slant-of-sunlight#:~:text=Barry%20Lopez%3A%20We%20are%20shaped,Country%20News%20–%20Know%20the%20West"
+DEFAULT_START = "in the"
+DEFAULT_END = "of community"
+DEFAULT_SENT = 3
 url = input('Article to summarize: ')
-# https://www.cnn.com/2022/09/20/sport/magnus-carlsen-hans-niemann-chess-spt-intl/index.html
+if url == 'd':
+    url = DEFAULT_LINK
 top_n = input('Summary length (in sentences): ')
+if top_n == 'd':
+    top_n = DEFAULT_SENT
 
 # Webscraping portion
 req = Request(
@@ -26,54 +32,89 @@ req = Request(
 html = urlopen(req).read().decode('utf8')
 
 # Limiting it down to actual content
-startstr = input("What are the first 2 words of the article? ").lower()  # shortly after
-endstr = input("What are the final 2 words of the article? ").lower()  # a solution
-raw = bs(html, 'html.parser').get_text().lower()
+startstr = input("What are the first 2 words of the article? ").lower()
+if startstr == 'd':
+    startstr = DEFAULT_START
+endstr = input("What are the final 2 words of the article? ").lower()
+if endstr == 'd':
+    endstr = DEFAULT_END
+print("\n\n")
+raw = bs(html, 'html.parser').get_text()
 raw = raw.replace(".", ". ")
-start = raw.find(startstr)
-end = raw.rfind(endstr)
+low_raw = raw.lower()
+start = low_raw.find(startstr.lower())
+end = low_raw.rfind(endstr.lower())
 text = raw[start:(end + len(endstr))]
 
 # Cleaning up text
 # text = re.sub(r'[0-9]+', ' ', text)  # replace all numbers
 # text = re.sub(r'\[[0-9]*\]', ' ', text)
 text = re.sub(r'\s+', ' ', text)  # replace all multiple spaces in a rows
-print(text)
-clean_text = text
+# print(text)
+clean_text = text.lower()
 # replace characters other than [a-zA-Z0-9], digits & one or more spaces with single space
 regex_patterns = [r'\W', r'\d', r'\s+']  # \W is any nonalpha numerical, \d is any number, \s+ is 1 or more whitespaces
 for regex in regex_patterns:
     clean_text = re.sub(regex, ' ', clean_text)
-print(clean_text)
+# print(clean_text)
 
 # Break into sentences
 sentences = nltk.sent_tokenize(text)
-print(sentences)
+# print(sentences)
 
-# todo maybe make this tfidf instead
-# Check word frequency
+# Check word importance
 stop = set(stopwords.words('english') + ["com"])
-fd = nltk.FreqDist(w.lower() for w in nltk.word_tokenize(clean_text) if w not in stop)
-print(dict(fd))
+clean_words = [w.lower() for w in word_tokenize(clean_text) if w not in stop]
+fd = FreqDist(clean_words)
+news_words = [w.lower() for fid in reuters.fileids() for w in reuters.words(fid) if w.isalpha()]
+# print(news_words)
+tf_news = TfidfVectorizer(use_idf=True, sublinear_tf=True, stop_words=stop)
+tf_news_doc = tf_news.fit_transform([' '.join(news_words)])
+tf = TfidfVectorizer(use_idf=True, sublinear_tf=True, stop_words=stop)
+tf_this = tf.fit_transform([' '.join(w for w in word_tokenize(text.lower()) if w.isalpha())])
 
-# sentence_score = {}
-# for sentence in sentences:
-#     for word in nltk.word_tokenize(sentence.lower()):
-#         if word in word_count.keys():
-#             if len(sentence.split(' ')) < MAX_SENT_LEN:
-#                 if sentence not in sentence_score.keys():
-#                     sentence_score[sentence] = word_count[word]
-#                 else:
-#                     sentence_score[sentence] += word_count[word]
+# Frequency scoring
+freq_sent_score = defaultdict(int)
+for sentence in sentences:
+    s_words = [w for w in word_tokenize(sentence.lower()) if w in fd]
+    if len(s_words) < MAX_SENT_LEN:
+        for word in s_words:
+            freq_sent_score[sentence] += fd[word]
+# Summary based on frequency
+print("Word-Frequency Based Summary:")
+freq_best_sents = heapq.nlargest(int(top_n), freq_sent_score, key=freq_sent_score.get)
+freq_summarized_text = ""
+for sentence in sentences:
+    if sentence in freq_best_sents:
+        freq_summarized_text += sentence + ' '
+print(freq_summarized_text, end="\n\n\n")
 
-# Generate summary
-# best_sentences = heapq.nlargest(int(top_n), sentence_score, key=sentence_score.get)
-# summarized_text = []
-# sentences = nltk.sent_tokenize(text)
-# for sentence in sentences:
-#     if sentence in best_sentences:  # TODO this seems stupid
-#         summarized_text.append(sentence)
-#
-# summary = "\n".join(summarized_text)
-#
-# print(summary)
+# Current news doc TFIDF scoring
+btf_sent_score = {}
+for sentence in sentences:
+    sent_len = len(word_tokenize(sentence))
+    if sent_len < MAX_SENT_LEN:
+        btf_sent_score[sentence] = sum(tf.transform([sentence]).data)
+# Summary based on current news doc TFIDF
+print("Current Article TFIDF Based Summary:")
+btf_best_sents = heapq.nlargest(int(top_n), btf_sent_score, key=btf_sent_score.get)
+btf_summarized_text = ""
+for sentence in sentences:
+    if sentence in btf_best_sents:
+        btf_summarized_text += sentence + ' '
+print(btf_summarized_text, end="\n\n\n")
+
+# News TFIDF scoring
+tf_sent_score = {}
+for sentence in sentences:
+    sent_len = len(word_tokenize(sentence))
+    if sent_len < MAX_SENT_LEN:
+        tf_sent_score[sentence] = sum(tf_news.transform([sentence]).data)
+# Summary based on current news doc TFIDF
+print("News Corpus TFIDF Based Summary:")
+tf_best_sents = heapq.nlargest(int(top_n), tf_sent_score, key=tf_sent_score.get)
+tf_summarized_text = ""
+for sentence in sentences:
+    if sentence in tf_best_sents:
+        tf_summarized_text += sentence + ' '
+print(tf_summarized_text)
--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-useful_words = ['hi', 'bye', 'die', 'hi']
-newDict = dict([(word, True) for word in useful_words])
-print(newDict)
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+hello = "chicken nuggets are cool chicken nuggets are cool chicken chicken are are are are are are"
+tf = TfidfVectorizer(use_idf=True, sublinear_tf=True)
+tf_yes = tf.fit_transform([hello])
+print(tf_yes, end="\n\n\n")
+
+something = tf.transform([" chicken cool chicken, chicken"])
+print(type(something))
+print(something)
+print(sum(something.data))
